6fc1c339b7b2036879c0505a7baaa9ccc8d96d0b
2604801 <brent.hamilton@cvshealth.com>
2024-07-22 18:41:03 -0700

    update router

--- frontend/fastapi-ui/src/main.js
+++ frontend/fastapi-ui/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
-    render: h => h(App)
+    render: h => h(App),
+    bootstrap
 }).$mount('#app');
